f5c3e8ba13fb50e7d00f2e7c3cc992488effd652
TakWolf <takwolf@foxmail.com>
2022-03-08 17:06:19 +0800

    遍历区块

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 谚文音节像素生成器
+
+AC00-D7AF; Hangul Syllables
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from utils import hangul_util
+
 
 def main():
-    print('Hello world!')
+    code_point = hangul_util.unicode_block_start
+    char_count = 0
+    for first_sound in hangul_util.first_sounds:
+        for middle_sound in hangul_util.middle_sounds:
+            # 双音节
+            print(f'U+{code_point:04X} {chr(code_point)} = {first_sound} {middle_sound}')
+            code_point += 1
+            char_count += 1
+            for last_sound in hangul_util.last_sounds:
+                # 三音节
+                print(f'U+{code_point:04X} {chr(code_point)} = {first_sound} {middle_sound} {last_sound}')
+                code_point += 1
+                char_count += 1
+    assert char_count == hangul_util.unicode_char_count
 
 
 if __name__ == '__main__':
